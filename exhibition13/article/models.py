e8cff6b0df8e66fcbe336e18910f354112089e57
AryaNazari86 <nazariarya86@gmail.com>
2021-10-16 09:53:51 +0330

    Files added

---
+++ exhibition13/article/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/articles/', default='images/default_article', blank=True)
+    video = models.FileField(upload_to='videos/articles', blank=True)
+    user = models.ForeignKey('account.User', on_delete=models.CASCADE)
